c6d5230c719b8cba393196497b63232a50fe02c4
Maarten Jacobs <m.jacobs@defacto.nl>
2020-01-24 12:58:19 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TestSelector.Mixfile do
   def project do
     [
       app: :test_selector,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.4",
       description: "Library to help selecting the right elements in your tests",
       build_embedded: Mix.env() == :test,
